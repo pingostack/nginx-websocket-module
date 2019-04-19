7f0e556182ef0f3cf5ade4ccf7ef10242b8a1942
pingox <cczjp89@gmail.com>
2019-04-19 09:56:28 +0800

    set the log level of echo module to debug

--- t/ngx_websocket_echo_module.c
+++ t/ngx_websocket_echo_module.c
@@ -73,7 +73,7 @@ ngx_websocket_echo_slot(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 static void
 ngx_websocket_connect_handler(ngx_websocket_session_t *ws)
 {
-    ngx_log_error(NGX_LOG_INFO, ws->log, 0,
+    ngx_log_error(NGX_LOG_DEBUG, ws->log, 0,
         "websocket-echo: connect_handler| new client connection.");
 
     ws->recv_handler = ngx_websocket_recv_handler;
@@ -87,18 +87,18 @@ ngx_websocket_recv_handler(ngx_websocket_session_t *ws,
     switch (opcode) {
         case NGX_WEBSOCKET_OPCODE_TEXT:
         case NGX_WEBSOCKET_OPCODE_BINARY:
-            ngx_log_error(NGX_LOG_INFO, ws->log, 0,
+            ngx_log_error(NGX_LOG_DEBUG, ws->log, 0,
                 "websocket-echo: recv_handler| recv: %V", msg);
             ngx_websocket_send_message(ws, msg, opcode);
         break;
 
         case NGX_WEBSOCKET_OPCODE_PONG:
-            ngx_log_error(NGX_LOG_INFO, ws->log, 0,
+            ngx_log_error(NGX_LOG_DEBUG, ws->log, 0,
                 "websocket-echo: recv_handler| getting pong ...");
         break;
 
         default:
-            ngx_log_error(NGX_LOG_INFO, ws->log, 0,
+            ngx_log_error(NGX_LOG_WARN, ws->log, 0,
                 "websocket-echo: recv_handler| drop message, opcode %d", opcode);
     }
 }
@@ -106,6 +106,6 @@ ngx_websocket_recv_handler(ngx_websocket_session_t *ws,
 static void
 ngx_websocket_disconnect_handler(ngx_websocket_session_t *ws)
 {
-    ngx_log_error(NGX_LOG_INFO, ws->log, 0,
+    ngx_log_error(NGX_LOG_DEBUG, ws->log, 0,
         "websocket-echo: disconnect_handler| client disconnect.");
 }
